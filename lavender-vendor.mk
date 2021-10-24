2055afce3cb1fee84496007a29bde80716620549
clarencelol <clarencekuiek@icloud.com>
2021-10-24 22:27:00 +0800

    lavender: Nuke datastatusnotification.apk
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -1726,7 +1726,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
